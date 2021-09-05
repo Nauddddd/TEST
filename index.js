8b9c2384447afe0fa53a64b1e48ce0bda4d2655b
new name <new email>
2021-09-06 04:50:43 +0700

    Add index.js file

---
+++ index.js
@@ -0,0 +1,33 @@
+const readline = require('readline').createInterface({
+	input: process.stdin,
+	output: process.stdout
+})
+
+readline.question(``, s => {
+	s = lengthOfLongestSubstring(s);
+	readline.close();
+});
+
+
+function lengthOfLongestSubstring(s) {
+    if (!s || s.length === 0){
+        return 0;
+    } else{
+		var max =0;
+		var temp_len;
+		var start = 0;
+		var cur;
+		for(cur = 0; cur < s.length; cur++) {
+			if(!(s.indexOf(s[cur], start) == cur)) { 
+				temp_len = cur - start;
+				start = s.indexOf(s[cur],start)+1;
+				if (temp_len > max)
+                	max = temp_len;
+			}
+		}
+	}
+	temp_len = cur - start;
+    if (temp_len > max)
+        max = temp_len;
+    return max;
+};
